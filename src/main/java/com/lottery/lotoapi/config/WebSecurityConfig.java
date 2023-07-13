fb274a0eeb37cd458734b935e03b62171bc3ab74
Vinícius Matiola Tramontin <viniciusmatiolatramontin@gmail.com>
2023-07-13 17:50:05 -0300

    Implemented OAuth2 with Github

--- src/main/java/com/lottery/lotoapi/config/WebSecurityConfig.java
+++ src/main/java/com/lottery/lotoapi/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.lottery.lotoapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,9 +15,10 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((requests) -> requests
-				.anyRequest().permitAll()
-			);
-
+				.anyRequest().authenticated()
+			)
+			.oauth2Login(Customizer.withDefaults());
+			
 		return http.build();
     }
 }
